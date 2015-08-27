8070d8b5163c0702c1279434fe75f5e3e2a1330f
Jesse Zelaya <jessezelaya1@gmail.com>
2015-08-26 22:40:13 -0700

    refactored code using both factory and controller

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -3,21 +3,42 @@
 //Where do you start? Maybe with an angular.module?
   angular
     .module('peopleApp', [])
-    // .factory("$MyFactory", myFactory)
+    .factory("$MyFactory", myFactory)
     .controller('PeopleController', peopleController);
 
+    //inject dependencies
+    myFactory.$inject = ["$http"];
+    //inject dependencies in controller
+    peopleController.$inject = ["$MyFactory"];
 
+    //factory function to get hoisted
+    function myFactory($http) {
+      var allPeopleUrl = "http://glenndama.herokuapp.com/api/people?limit=5";
+      var peopleFactory = {};
 
+      peopleFactory.getFivePeople = function (people) {
+        return $http.get(allPeopleUrl);
+      }
 
-    function peopleController($http) {
+      return peopleFactory;
+    }
+
+    //people controller to get hoisted
+    function peopleController($MyFactory) {
       var vm = this;
       vm.people;
+      vm.api = $MyFactory;
 
-      //get all people
-      $http.get("http://glenndama.herokuapp.com/api/people?limit=5")
+      vm.api.getFivePeople()
         .success(function (data) {
-          vm.people = (data);
-          console.log(vm.people);
+          vm.people = (data)
         })
+
+      // //get all people
+      // $http.get("http://glenndama.herokuapp.com/api/people?limit=5")
+      //   .success(function (data) {
+      //     vm.people = (data);
+      //     console.log(vm.people);
+      //   })
     }//End peopleController
 })();//End IIFE
