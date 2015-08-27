e93037b1101f6a4bd49bdd4f80726a857f8f6fc0
Jesse Zelaya <jessezelaya1@gmail.com>
2015-08-26 22:33:29 -0700

    created controller to display up to five people

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -1,4 +1,23 @@
+//app.js
+(function() { //IIFE
 //Where do you start? Maybe with an angular.module?
-//...then a factory?
-//..then controllers?
-//..did you remember to inject your dependencies?
+  angular
+    .module('peopleApp', [])
+    // .factory("$MyFactory", myFactory)
+    .controller('PeopleController', peopleController);
+
+
+
+
+    function peopleController($http) {
+      var vm = this;
+      vm.people;
+
+      //get all people
+      $http.get("http://glenndama.herokuapp.com/api/people?limit=5")
+        .success(function (data) {
+          vm.people = (data);
+          console.log(vm.people);
+        })
+    }//End peopleController
+})();//End IIFE
